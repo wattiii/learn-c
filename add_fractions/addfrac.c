322a2e19eb1377e9bc9bc2bf10f0fd0b2e2691bc
watt.iii.2021@gmail.com <97683167+wattiii@users.noreply.github.com>
2026-07-17 20:18:11 -0400

    addfrac.c

---
+++ add_fractions/addfrac.c
@@ -0,0 +1,20 @@
+/* add two fractions */
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, denom1, num2, denom2, result_num, result_denom;
+
+    printf("Enter the first fraction (numerator and denominator): ");
+    scanf("%d %d", &num1, &denom1);
+
+    printf("Enter the second fraction (numerator and denominator): ");
+    scanf("%d %d", &num2, &denom2);
+
+    result_num = num1 * denom2 + num2 * denom1;
+    result_denom = denom1 * denom2;
+
+    printf("The sum of the fractions is: %d/%d\n", result_num, result_denom);
+
+    return 0;
+}
