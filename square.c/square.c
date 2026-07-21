75b1ae8785236e86896996d36d624363aacc3ee8
watt.iii.2021@gmail.com <97683167+wattiii@users.noreply.github.com>
2026-07-21 08:06:35 -0400

    square.c

---
+++ square.c/square.c
@@ -0,0 +1,17 @@
+/*   print a table of squares */
+#include <stdio.h>
+
+int main(void)
+{
+    int i, n;
+    printf("This program prints a table of squares.\n");
+    printf("Enter the number of entries in the table: ");
+    scanf("%d", &n);
+    i = 1;
+    while (i <= n)
+    {
+        printf("%10d%10d\n", i, i * i); // print the square of i
+        i++;
+    }
+    return 0;
+}
